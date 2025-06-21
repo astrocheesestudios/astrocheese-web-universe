528ac11c36e913888083ad67f4cbbd4746a84b1f
astrocheesestudios <management@astrocheesestudios.com>
2025-06-21 11:02:04 -0700

    [MAJOR] Render a cube

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Imports
+import * as THREE from 'three';
+const canvas = document.getElementById('three-canvas');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+});
+renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+renderer.setAnimationLoop(animate);
+
+// Create a simple cube geometry with a width, height, and depth of 1 unit
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+// Create a render loop that will continuously update the scene
+function animate() {
+  cube.rotation.x += 0.01; // Rotate the cube around the x-axis
+  cube.rotation.y += 0.01; // Rotate the cube around the y-axis
+  cube.rotation.z += 0.01; // Rotate the cube around the z-axis
+    renderer.render(scene, camera);
+}
